882653672461ddbf2b65afc3929eae8c1ee739ba
Dillon Chanis <dillonchanis92@gmail.com>
2018-04-09 20:11:34 -0400

    Stubbing out render logic

--- src/components/MediaQuery.vue
+++ src/components/MediaQuery.vue
@@ -3,7 +3,13 @@ export default {
   name: 'mediaQuery',
   props: {
     at: {
-      type: [String, Number]
+      type: [String, Number],
+      required: true
+    },
+    display: {
+      type: String,
+      default: 'visible',
+      validator: value => ['visible', 'hidden'].includes(value)
     },
     maxWidth: {
       type: Boolean,
@@ -16,7 +22,12 @@ export default {
     unit: {
       type: String,
       default: 'px',
-      validator: value => ['px', '%'].includes(value)
+      validator: value => ['px', 'em'].includes(value)
+    }
+  },
+  data () {
+    return {
+      width: null
     }
   },
   mounted () {
@@ -28,16 +39,31 @@ export default {
   },
   methods: {
     onResize () {
-      // todo
+      this.width = this.unit === 'px'
+        ? window.innerWidth
+        : window.innerWidth / 16
     }
   },
   render () {
+    if (!this.maxWidth && !this.minWidth) {
+      console.warn(`You must set one of the props, 'max-width' or 'min-width' to true.`)
+      return
+    }
+
     if (this.maxWidth && this.minWidth) {
       console.warn(`The props 'max-width' and 'min-width' are active when only one is allowed.`)
       return
     }
 
-    return this.$slots.default[0]
+    let toRender = false
+
+    if ((this.maxWidth && this.display === 'hidden') || (this.minWidth && this.display === 'visible')) {
+      toRender = this.width < this.at
+    } else {
+      toRender = this.width > this.at
+    }
+
+    return toRender ? this.$slots.default[0] : null
   }
 }
 </script>
